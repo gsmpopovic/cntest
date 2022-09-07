be7a1e12440d775eee12d48b0cfe129d9a4fde3d
George Popovic <georgesmpopovic@gmail.com>
2022-09-06 20:02:28 -0400

    Contact form views.

---
+++ src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="contact-form">
+      <ContactForm/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import ContactForm from '@/components/ContactForm.vue'
+  
+  export default {
+    name: 'ContactView',
+    components: {
+      ContactForm
+    }
+  }
+  </script>
